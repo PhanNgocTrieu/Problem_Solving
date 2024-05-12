6163f456b6d78359abc153c8c07cc17750d44455
trieupn <pntrieu200799@gmail.com>
2024-05-13 00:58:41 +0700

    Update files for serve God

--- _leet_code/src/easyLevel.cpp
+++ _leet_code/src/easyLevel.cpp
@@ -671,29 +671,20 @@ namespace leetcode
     }
 
     vector<vector<int>> easyLevel::largestLocal(vector<vector<int>>& grid) {
-        vector<vector<int>> result;
-        int _count = 0;
-        int s_r = 0;
-        int s_c = 0;
-        do {
-            vector<int> temp;
-            /* Finding max in 3x3 matrix */
-            {
-                int max = -1;
-                {
-                    for (int i = s_i; i < s_i + 2; i++) {
-                        int s_j = 0;   
-                        for (int j = s_j; j < s_j + 2; j++) {
-                            if (max < grid[i][j]) {
-                                max = grid[i][j];
-                            }
-                            s_j++;
-                        }
-                        s_i++;
+        int n = grid.size();
+        vector<vector<int>> res(n - 2, vector<int>(n - 2));
+        for(int i = 1; i < n - 1; ++i) {
+            for(int j = 1; j < n - 1; ++j) {
+                int temp = 0;
+
+                for(int k = i - 1; k <= i + 1; ++k) {
+                    for(int l = j - 1; l <= j + 1; ++l) {
+                        temp = max(temp, grid[k][l]);
                     }
                 }
-                temp.push_back(max);
+
+                res[i - 1][j - 1] = temp;
             }
-        } while ( s_r < (grid.size() - 2) );
+        }
     }
 };  
