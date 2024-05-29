7e3f711637096ede0e000fc375adc4fc940e01c5
trieupn1 <pntrieu200799@gmail.com>
2024-05-29 23:21:04 +0700

    Update files of problems

--- update.sh
+++ update.sh
@@ -1,22 +1,21 @@
 echo "Processing of Update Github"
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    *)          machine="UNKNOWN:${unameOut}"
-esac
 
-echo "Operating System: " ${machine}
-
-if [ "$machine" == "Mac" ]; then
-    echo "Removing all .DS_Store files from MacOS"
-    find . -name ".DS_Store" -exec rm {} \;
-fi
+# unameOut="$(uname -s)"
+# case "${unameOut}" in
+#     Linux*)     machine=Linux;;
+#     Darwin*)    machine=Mac;;
+#     CYGWIN*)    machine=Cygwin;;
+#     MINGW*)     machine=MinGw;;
+#     *)          machine="UNKNOWN:${unameOut}"
+# esac
+# echo "Operating System: " ${machine}
+# if [ "$machine" == "Mac" ]; then
+#     echo "Removing all .DS_Store files from MacOS"
+#     find . -name ".DS_Store" -exec rm {} \;
+# fi
 
 git pull
 git add .
-git commit -m "Update files for serve God"
+git commit -m "Update files of problems"
 git push origin master
 echo "Finished Process of Updated"
