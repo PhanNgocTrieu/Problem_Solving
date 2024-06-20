b0ce546cb8699da9a3481dd37b4c88a0a3808629
Trieu John <pntrieu200799@gmail.com>
2024-06-20 11:38:03 +0700

    Create common.h

---
+++ _leet_code/weekly/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+using namespace std;
+
+#define loop(I,S) for(int I = 0; I < S; ++I) \
+
+#endif
