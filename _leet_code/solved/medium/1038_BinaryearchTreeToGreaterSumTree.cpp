db56bef75ff879ef27dcad76849272a782f17a6e
Trieu John <pntrieu200799@gmail.com>
2024-06-26 09:08:27 +0700

    Create 1038_BinaryearchTreeToGreaterSumTree.cpp

---
+++ _leet_code/solved/medium/1038_BinaryearchTreeToGreaterSumTree.cpp
@@ -0,0 +1,61 @@
+/*
+    Given the root of a Binary Search Tree (BST), 
+    convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+    As a reminder, a binary search tree is a tree that satisfies these constraints:
+    
+    The left subtree of a node contains only nodes with keys less than the node's key.
+    The right subtree of a node contains only nodes with keys greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
+
+    Example 1:
+        Original
+                4
+              /   \
+            1       6
+          /   \    /  \
+        0      2  5    7 
+                \       \
+                 3       8 
+
+         Result:
+                 4 (30)
+              /    \
+            1 (36)   6 (21)
+          /   \     /  \
+    (36)0  (35)2  5(26)  7 (15)
+                \        \
+                 3 (33)   8 (8)
+                 
+        Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+        Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+        Example 2:
+        
+        Input: root = [0,null,1]
+        Output: [1,null,1]
+
+    
+*/
+class Solution {
+    public:
+        TreeNode* bstToGst(TreeNode* root) {
+            // Initialize a local variable nodeSum.
+            int nodeSum = 0;
+            bstToGstHelper(root, nodeSum);
+            return root;
+        }
+    
+    private:
+        void bstToGstHelper(TreeNode* root, int& nodeSum) {
+            // If root is null, make no changes.
+            if (!root) {
+                return;
+            }
+    
+            bstToGstHelper(root->right, nodeSum);
+            nodeSum += root->val;
+            // Update the value of root.
+            root->val = nodeSum;
+            bstToGstHelper(root->left, nodeSum);
+        }
+};
