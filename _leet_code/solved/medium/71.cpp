5b63d2370e62ab29daf9d1b1798399e19d0148cb
Trieu John <pntrieu200799@gmail.com>
2024-07-16 17:23:05 +0700

    Update 71.cpp

--- _leet_code/solved/medium/71.cpp
+++ _leet_code/solved/medium/71.cpp
@@ -58,3 +58,28 @@ string simplifyPath(string path) {
 
     return ans[ans.length() - 1] == '/' && ans.size() > 1 ? ans.substr(0, ans.length() - 1) : ans;
 }
+
+
+//Solution 01:
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> s;
+        string ans, temp;
+        int n = path.size();
+        stringstream X(path);
+        
+        while(getline(X, temp, '/')){
+            if(temp == "" || temp == ".") continue;
+            if(temp != ".." ) s.push(temp);
+            else if(!s.empty()) s.pop();
+        }
+        if(s.empty()) return "/";
+        
+        while(!s.empty()){
+            ans = '/' + s.top() + ans;
+            s.pop();
+        }
+        return ans;
+    }
+};
